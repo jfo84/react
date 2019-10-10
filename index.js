d5436fa13bbb901559d84070a333bc2a40fdbc97
AT-MBP-47 <jford8820@gmail.com>
2019-10-10 10:51:19 +0200

    Point to react-dom dist

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./build/dist/react.production.min.js');
+  module.exports = require('./build/dist/react-dom.production.min.js');
 } else {
-  module.exports = require('./build/dist/react.development.js');
+  module.exports = require('./build/dist/react-dom.development.js');
 }
